69ace7376683fb9255474f16e16022e713c8901a
will <gunmonyk@gmail.com>
2024-10-04 17:33:34 +0200

    changing names, buttons functions for menu, directory button icons added.

--- Gui/main_window.py
+++ Gui/main_window.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import QSize
 from functools import partial
-from PIL import Image
 
-from Qt_files.ui_untitled import Ui_MainWindow
+from Qt_files.ui_Green_wall import Ui_MainWindow
 from Device_communication.logo import LogoControl
 
 
@@ -20,12 +20,19 @@ class MainWindow(QMainWindow):
 
     def _init_graphical_changes(self):
         self.ui.logo_4j_lbl.setPixmap(QPixmap('./App_data/4j_logo_150x50.png'))
+        self.ui.solution_dir_btn.setIcon(QIcon("./App_data/dir_icon.png"))
+        self.ui.solution_dir_btn.setIconSize(QSize(54, 30))
+        self.ui.cam_dir_btn.setIcon(QIcon("./App_data/dir_icon.png"))
+        self.ui.cam_dir_btn.setIconSize(QSize(54, 30))
 
         self.setWindowIcon(QIcon("./App_data/ico.png"))
         self.setWindowTitle("Pneu Friction")
-        self.setMinimumSize(1500, 800)
+        self.setMinimumSize(1500, 900)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.watering_pg)
         self.showMaximized()
 
     def button_functions(self):
-        pass
-
+        self.ui.watering_page_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.watering_pg))
+        self.ui.solutio_page_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.solution_pg))
+        self.ui.cams_page_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.camera_pg))
+        self.ui.chart_page_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.chart_pg))
--- Qt_files/ui_Green_wall.py
+++ Qt_files/ui_Green_wall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'untitledcCZlmt.ui'
+## Form generated from reading UI file 'Green_wallfFFpLQ.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
@@ -55,29 +55,29 @@ class Ui_MainWindow(object):
         self.widget_4.setObjectName(u"widget_4")
         self.verticalLayout_3 = QVBoxLayout(self.widget_4)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.pushButton = QPushButton(self.widget_4)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setMaximumSize(QSize(16777215, 40))
+        self.watering_page_btn = QPushButton(self.widget_4)
+        self.watering_page_btn.setObjectName(u"watering_page_btn")
+        self.watering_page_btn.setMaximumSize(QSize(16777215, 40))
 
-        self.verticalLayout_3.addWidget(self.pushButton)
+        self.verticalLayout_3.addWidget(self.watering_page_btn)
 
-        self.pushButton_2 = QPushButton(self.widget_4)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setMaximumSize(QSize(16777215, 40))
+        self.solutio_page_btn = QPushButton(self.widget_4)
+        self.solutio_page_btn.setObjectName(u"solutio_page_btn")
+        self.solutio_page_btn.setMaximumSize(QSize(16777215, 40))
 
-        self.verticalLayout_3.addWidget(self.pushButton_2)
+        self.verticalLayout_3.addWidget(self.solutio_page_btn)
 
-        self.pushButton_3 = QPushButton(self.widget_4)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setMaximumSize(QSize(16777215, 40))
+        self.cams_page_btn = QPushButton(self.widget_4)
+        self.cams_page_btn.setObjectName(u"cams_page_btn")
+        self.cams_page_btn.setMaximumSize(QSize(16777215, 40))
 
-        self.verticalLayout_3.addWidget(self.pushButton_3)
+        self.verticalLayout_3.addWidget(self.cams_page_btn)
 
-        self.pushButton_4 = QPushButton(self.widget_4)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setMaximumSize(QSize(16777215, 40))
+        self.chart_page_btn = QPushButton(self.widget_4)
+        self.chart_page_btn.setObjectName(u"chart_page_btn")
+        self.chart_page_btn.setMaximumSize(QSize(16777215, 40))
 
-        self.verticalLayout_3.addWidget(self.pushButton_4)
+        self.verticalLayout_3.addWidget(self.chart_page_btn)
 
 
         self.verticalLayout.addWidget(self.widget_4)
@@ -97,116 +97,116 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
         self.stackedWidget = QStackedWidget(self.widget_2)
         self.stackedWidget.setObjectName(u"stackedWidget")
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.gridLayout = QGridLayout(self.page)
+        self.watering_pg = QWidget()
+        self.watering_pg.setObjectName(u"watering_pg")
+        self.gridLayout = QGridLayout(self.watering_pg)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.widget_26 = QWidget(self.page)
+        self.widget_26 = QWidget(self.watering_pg)
         self.widget_26.setObjectName(u"widget_26")
 
         self.gridLayout.addWidget(self.widget_26, 4, 1, 1, 1)
 
-        self.widget_28 = QWidget(self.page)
+        self.widget_28 = QWidget(self.watering_pg)
         self.widget_28.setObjectName(u"widget_28")
 
         self.gridLayout.addWidget(self.widget_28, 6, 1, 1, 1)
 
-        self.widget_21 = QWidget(self.page)
+        self.widget_21 = QWidget(self.watering_pg)
         self.widget_21.setObjectName(u"widget_21")
 
         self.gridLayout.addWidget(self.widget_21, 5, 0, 1, 1)
 
-        self.widget_17 = QWidget(self.page)
+        self.widget_17 = QWidget(self.watering_pg)
         self.widget_17.setObjectName(u"widget_17")
 
         self.gridLayout.addWidget(self.widget_17, 2, 1, 1, 1)
 
-        self.widget_31 = QWidget(self.page)
+        self.widget_31 = QWidget(self.watering_pg)
         self.widget_31.setObjectName(u"widget_31")
 
         self.gridLayout.addWidget(self.widget_31, 4, 2, 1, 1)
 
-        self.widget_33 = QWidget(self.page)
+        self.widget_33 = QWidget(self.watering_pg)
         self.widget_33.setObjectName(u"widget_33")
 
         self.gridLayout.addWidget(self.widget_33, 6, 2, 1, 1)
 
-        self.widget_24 = QWidget(self.page)
+        self.widget_24 = QWidget(self.watering_pg)
         self.widget_24.setObjectName(u"widget_24")
 
         self.gridLayout.addWidget(self.widget_24, 1, 2, 1, 1)
 
-        self.widget_15 = QWidget(self.page)
+        self.widget_15 = QWidget(self.watering_pg)
         self.widget_15.setObjectName(u"widget_15")
 
         self.gridLayout.addWidget(self.widget_15, 2, 0, 1, 1)
 
-        self.widget_22 = QWidget(self.page)
+        self.widget_22 = QWidget(self.watering_pg)
         self.widget_22.setObjectName(u"widget_22")
 
         self.gridLayout.addWidget(self.widget_22, 6, 0, 1, 1)
 
-        self.widget_29 = QWidget(self.page)
+        self.widget_29 = QWidget(self.watering_pg)
         self.widget_29.setObjectName(u"widget_29")
 
         self.gridLayout.addWidget(self.widget_29, 7, 1, 1, 1)
 
-        self.widget_18 = QWidget(self.page)
+        self.widget_18 = QWidget(self.watering_pg)
         self.widget_18.setObjectName(u"widget_18")
 
         self.gridLayout.addWidget(self.widget_18, 3, 1, 1, 1)
 
-        self.widget_19 = QWidget(self.page)
+        self.widget_19 = QWidget(self.watering_pg)
         self.widget_19.setObjectName(u"widget_19")
 
         self.gridLayout.addWidget(self.widget_19, 3, 0, 1, 1)
 
-        self.widget_27 = QWidget(self.page)
+        self.widget_27 = QWidget(self.watering_pg)
         self.widget_27.setObjectName(u"widget_27")
 
         self.gridLayout.addWidget(self.widget_27, 5, 1, 1, 1)
 
-        self.widget_14 = QWidget(self.page)
+        self.widget_14 = QWidget(self.watering_pg)
         self.widget_14.setObjectName(u"widget_14")
 
         self.gridLayout.addWidget(self.widget_14, 1, 0, 1, 1)
 
-        self.widget_30 = QWidget(self.page)
+        self.widget_30 = QWidget(self.watering_pg)
         self.widget_30.setObjectName(u"widget_30")
 
         self.gridLayout.addWidget(self.widget_30, 3, 2, 1, 1)
 
-        self.widget_32 = QWidget(self.page)
+        self.widget_32 = QWidget(self.watering_pg)
         self.widget_32.setObjectName(u"widget_32")
 
         self.gridLayout.addWidget(self.widget_32, 5, 2, 1, 1)
 
-        self.widget_23 = QWidget(self.page)
+        self.widget_23 = QWidget(self.watering_pg)
         self.widget_23.setObjectName(u"widget_23")
 
         self.gridLayout.addWidget(self.widget_23, 7, 0, 1, 1)
 
-        self.widget_34 = QWidget(self.page)
+        self.widget_34 = QWidget(self.watering_pg)
         self.widget_34.setObjectName(u"widget_34")
 
         self.gridLayout.addWidget(self.widget_34, 7, 2, 1, 1)
 
-        self.widget_25 = QWidget(self.page)
+        self.widget_25 = QWidget(self.watering_pg)
         self.widget_25.setObjectName(u"widget_25")
 
         self.gridLayout.addWidget(self.widget_25, 2, 2, 1, 1)
 
-        self.widget_20 = QWidget(self.page)
+        self.widget_20 = QWidget(self.watering_pg)
         self.widget_20.setObjectName(u"widget_20")
 
         self.gridLayout.addWidget(self.widget_20, 4, 0, 1, 1)
 
-        self.widget_16 = QWidget(self.page)
+        self.widget_16 = QWidget(self.watering_pg)
         self.widget_16.setObjectName(u"widget_16")
 
         self.gridLayout.addWidget(self.widget_16, 1, 1, 1, 1)
 
-        self.widget_48 = QWidget(self.page)
+        self.widget_48 = QWidget(self.watering_pg)
         self.widget_48.setObjectName(u"widget_48")
         self.widget_48.setMinimumSize(QSize(0, 35))
         self.widget_48.setMaximumSize(QSize(16777215, 35))
@@ -228,12 +228,12 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.widget_48, 0, 0, 1, 3)
 
-        self.stackedWidget.addWidget(self.page)
-        self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
-        self.verticalLayout_8 = QVBoxLayout(self.page_3)
+        self.stackedWidget.addWidget(self.watering_pg)
+        self.solution_pg = QWidget()
+        self.solution_pg.setObjectName(u"solution_pg")
+        self.verticalLayout_8 = QVBoxLayout(self.solution_pg)
         self.verticalLayout_8.setObjectName(u"verticalLayout_8")
-        self.widget_10 = QWidget(self.page_3)
+        self.widget_10 = QWidget(self.solution_pg)
         self.widget_10.setObjectName(u"widget_10")
         self.widget_10.setMaximumSize(QSize(16777215, 80))
         self.horizontalLayout_4 = QHBoxLayout(self.widget_10)
@@ -249,11 +249,11 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_4.addWidget(self.lineEdit_2)
 
-        self.pushButton_7 = QPushButton(self.widget_10)
-        self.pushButton_7.setObjectName(u"pushButton_7")
-        self.pushButton_7.setMinimumSize(QSize(35, 35))
+        self.solution_dir_btn = QPushButton(self.widget_10)
+        self.solution_dir_btn.setObjectName(u"solution_dir_btn")
+        self.solution_dir_btn.setMinimumSize(QSize(35, 35))
 
-        self.horizontalLayout_4.addWidget(self.pushButton_7)
+        self.horizontalLayout_4.addWidget(self.solution_dir_btn)
 
         self.pushButton_6 = QPushButton(self.widget_10)
         self.pushButton_6.setObjectName(u"pushButton_6")
@@ -264,7 +264,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_8.addWidget(self.widget_10)
 
-        self.widget_35 = QWidget(self.page_3)
+        self.widget_35 = QWidget(self.solution_pg)
         self.widget_35.setObjectName(u"widget_35")
         self.widget_35.setMinimumSize(QSize(0, 200))
         self.horizontalLayout_5 = QHBoxLayout(self.widget_35)
@@ -287,27 +287,27 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_8.addWidget(self.widget_35)
 
-        self.widget_12 = QWidget(self.page_3)
+        self.widget_12 = QWidget(self.solution_pg)
         self.widget_12.setObjectName(u"widget_12")
 
         self.verticalLayout_8.addWidget(self.widget_12)
 
-        self.widget_36 = QWidget(self.page_3)
+        self.widget_36 = QWidget(self.solution_pg)
         self.widget_36.setObjectName(u"widget_36")
 
         self.verticalLayout_8.addWidget(self.widget_36)
 
-        self.widget_37 = QWidget(self.page_3)
+        self.widget_37 = QWidget(self.solution_pg)
         self.widget_37.setObjectName(u"widget_37")
 
         self.verticalLayout_8.addWidget(self.widget_37)
 
-        self.stackedWidget.addWidget(self.page_3)
-        self.page_4 = QWidget()
-        self.page_4.setObjectName(u"page_4")
-        self.horizontalLayout_6 = QHBoxLayout(self.page_4)
+        self.stackedWidget.addWidget(self.solution_pg)
+        self.chart_pg = QWidget()
+        self.chart_pg.setObjectName(u"chart_pg")
+        self.horizontalLayout_6 = QHBoxLayout(self.chart_pg)
         self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
-        self.widget_41 = QWidget(self.page_4)
+        self.widget_41 = QWidget(self.chart_pg)
         self.widget_41.setObjectName(u"widget_41")
         self.verticalLayout_9 = QVBoxLayout(self.widget_41)
         self.verticalLayout_9.setObjectName(u"verticalLayout_9")
@@ -325,7 +325,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_6.addWidget(self.widget_41)
 
-        self.widget_42 = QWidget(self.page_4)
+        self.widget_42 = QWidget(self.chart_pg)
         self.widget_42.setObjectName(u"widget_42")
         self.verticalLayout_10 = QVBoxLayout(self.widget_42)
         self.verticalLayout_10.setObjectName(u"verticalLayout_10")
@@ -348,12 +348,12 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_6.addWidget(self.widget_42)
 
-        self.stackedWidget.addWidget(self.page_4)
-        self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
-        self.verticalLayout_5 = QVBoxLayout(self.page_2)
+        self.stackedWidget.addWidget(self.chart_pg)
+        self.camera_pg = QWidget()
+        self.camera_pg.setObjectName(u"camera_pg")
+        self.verticalLayout_5 = QVBoxLayout(self.camera_pg)
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
-        self.widget_6 = QWidget(self.page_2)
+        self.widget_6 = QWidget(self.camera_pg)
         self.widget_6.setObjectName(u"widget_6")
         self.widget_6.setMaximumSize(QSize(16777215, 80))
         self.horizontalLayout_3 = QHBoxLayout(self.widget_6)
@@ -364,11 +364,11 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.lineEdit)
 
-        self.pushButton_8 = QPushButton(self.widget_6)
-        self.pushButton_8.setObjectName(u"pushButton_8")
-        self.pushButton_8.setMinimumSize(QSize(35, 35))
+        self.cam_dir_btn = QPushButton(self.widget_6)
+        self.cam_dir_btn.setObjectName(u"cam_dir_btn")
+        self.cam_dir_btn.setMinimumSize(QSize(35, 35))
 
-        self.horizontalLayout_3.addWidget(self.pushButton_8)
+        self.horizontalLayout_3.addWidget(self.cam_dir_btn)
 
         self.pushButton_5 = QPushButton(self.widget_6)
         self.pushButton_5.setObjectName(u"pushButton_5")
@@ -380,7 +380,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_5.addWidget(self.widget_6)
 
-        self.widget_7 = QWidget(self.page_2)
+        self.widget_7 = QWidget(self.camera_pg)
         self.widget_7.setObjectName(u"widget_7")
         self.horizontalLayout_2 = QHBoxLayout(self.widget_7)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
@@ -430,7 +430,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_5.addWidget(self.widget_7)
 
-        self.stackedWidget.addWidget(self.page_2)
+        self.stackedWidget.addWidget(self.camera_pg)
 
         self.verticalLayout_4.addWidget(self.stackedWidget)
 
@@ -440,7 +440,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1264, 23))
+        self.menubar.setGeometry(QRect(0, 0, 1264, 24))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -457,16 +457,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.logo_4j_lbl.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Zal\u00e9v\u00e1n\u00ed", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Nastaven\u00ed roztoku", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Kamery", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Grafy", None))
+        self.watering_page_btn.setText(QCoreApplication.translate("MainWindow", u"Zal\u00e9v\u00e1n\u00ed", None))
+        self.solutio_page_btn.setText(QCoreApplication.translate("MainWindow", u"Nastaven\u00ed roztoku", None))
+        self.cams_page_btn.setText(QCoreApplication.translate("MainWindow", u"Kamery", None))
+        self.chart_page_btn.setText(QCoreApplication.translate("MainWindow", u"Grafy", None))
         self.pushButton_9.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.pushButton_10.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.pushButton_7.setText("")
+        self.solution_dir_btn.setText("")
         self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"Ulo\u017eit do csv", None))
-        self.pushButton_8.setText("")
+        self.cam_dir_btn.setText("")
         self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"Ulo\u017eit sn\u00edmek", None))
     # retranslateUi
 
